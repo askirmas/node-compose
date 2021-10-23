9a0dba048704c3c664fce74793c536d5a30ec143
Andrii Kirmas <kirmas@phystech.edu>
2021-10-23 12:49:32 +0300

    Dive deeper to function `PerformanceEntry`

--- src/research.test.ts
+++ src/research.test.ts
@@ -1,24 +1,48 @@
 import {
   performance,
+  PerformanceEntry,
   PerformanceObserver
 } from 'perf_hooks'
+import { matchers } from 'jest-json-schema';
 
-const observed: unknown[] = []
+expect.extend(matchers);
+const observed: PerformanceEntry[] = []
 
-const obs = new PerformanceObserver((list) => {
-  observed.push(list.getEntries()[0].duration);
-  obs.disconnect();
+const obs = new PerformanceObserver(list => {
+  const entries = list.getEntries()
+  observed.push(...entries);
 });
-obs.observe({ entryTypes: ['function'] });
+obs.observe({
+  "entryTypes": [
+    "function"
+  ],
+  // buffered: true
+});
 
-
+afterAll(() =>  obs.disconnect())
 /** @see https://nodejs.org/api/perf_hooks.html#performancetimerifyfn-options  */
 it("basic wrap", () => {
   const wrapped = performance.timerify(someFunction);
   // A performance timeline entry will be created
   wrapped();
   expect(observed).toHaveLength(1)  
-  expect(observed[0]).toBeLessThan(0.1)
+  const {
+    duration,
+    entryType,
+    name,
+    // startTime,
+    detail
+  } = observed[0]
+  expect({
+    entryType,
+    name,
+    detail
+  }).toStrictEqual({
+    "entryType": "function",
+    "name": someFunction.name,
+    "detail": undefined
+  })
+  expect(duration).toBeLessThan(0.1)
 })
 
 function someFunction() {
